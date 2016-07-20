bccfed8e8a81224189191b3b92a3c47aba3965af
Florian Diegruber <florian@diegruber.net>
2016-07-20 18:24:43 +0200

    activity can be displayed, no information yet

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -7,26 +7,33 @@ end
 
 
 def create
-  activity = Activity.new(
-    :name => params[:activity][:name].capitalize,
-    :participants => params[:activity][:number],
-    :groups => params[:activity][:groups])
+  activity = Activity.new(activity_params)
+  # activity = Activity.new(
+  #   :name => params[:activity][:name].capitalize,
+  #   :participants => params[:activity][:number],
+  #   :groups => params[:activity][:groups])
 
   if
   activity.name =~ /^$/ || activity.participants =~ /^$/
 
-  redirect_to("/new")
+  redirect_to("activity/new")
   else
   activity.users << current_user
   activity.save
-  redirect_to("/activity")
+  redirect_to ("/activity/#{activity.id}")
   end
 end
 
 
-def index
+def show
+@activity = Activity.find_by(id: params[:id])
 
+end
 
+private
+
+def activity_params
+  params.require(:activity).permit(:name, :participants, :groups)
 end
 
 

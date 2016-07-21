efba2db05bfb040dfc7d47a91f5b20c4ab568ffa
Florian Diegruber <florian@diegruber.net>
2016-07-21 19:54:03 +0200

    heroku update

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160720115440) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "activities", force: :cascade do |t|
     t.string   "name"
     t.integer  "participants"
@@ -25,8 +28,8 @@ ActiveRecord::Schema.define(version: 20160720115440) do
     t.integer  "activity_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["activity_id"], name: "index_user_activities_on_activity_id"
-    t.index ["user_id"], name: "index_user_activities_on_user_id"
+    t.index ["activity_id"], name: "index_user_activities_on_activity_id", using: :btree
+    t.index ["user_id"], name: "index_user_activities_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|

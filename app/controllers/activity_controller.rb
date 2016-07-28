6cd79cda4f47792d5cc068488749a5dd7e55e8c7
Florian Diegruber <florian@diegruber.net>
2016-07-29 00:51:59 +0200

    vote counter added

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,84 +1,90 @@
 class ActivityController < ApplicationController
 before_action :authorize_user
 
-def new
-end
-
-
-def create
-
-activity = Activity.new(activity_params)
-
-
-  if
-  activity.name =~ /^$/
-  # || activity.participants =~ /^$/
-
-  redirect_to("activity/new")
-  else
-  activity.users << current_user
-  datevote = DateEntry.create(:date => activity.starts_at)
-  activity.date_entries << datevote
-  activity.save
-  redirect_to ("/activity/#{activity.id}")
+  def new
   end
-end
-
-def update
-  activity = Activity.find_by(id: params[:id])
-  activity.update_attributes!(activity_params)
-  redirect_to ("/activity/#{activity.id}")
-end
 
 
-def show
-@activity = Activity.find_by(id: params[:id])
-@date_entry = DateEntry.new
-@comment = Comment.new
+  def create
 
-end
+  activity = Activity.new(activity_params)
 
 
+    if
+    activity.name =~ /^$/
+    # || activity.participants =~ /^$/
 
-def voting
-
-activity = Activity.find_by(id: params[:actid])
-unchecked = params[:uncheckedboxes]
-checked = params[:checkedboxes]
-
-
-
-checked.each do |vote|
-  if DateVote.exists?(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
-  datevotechecked  = DateVote.find_by(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
-  DateVote.update(datevotechecked.id, check: true)
-  else
-  @datevotechecked = DateVote.new(check: true)
-  @datevotechecked.activity_id = activity.id
-  @datevotechecked.user_id = current_user.id
-  @datevotechecked.date_entry_id = vote
-  @datevotechecked.save
+    redirect_to("activity/new")
+    else
+    activity.users << current_user
+    datevote = DateEntry.create(:date => activity.starts_at)
+    activity.date_entries << datevote
+    activity.save
+    redirect_to ("/activity/#{activity.id}")
+    end
   end
-end
-unchecked.each do |vote|
-  if DateVote.exists?(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
-  datevoteunchecked  = DateVote.find_by(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
-  DateVote.update(datevoteunchecked.id,check: false)
+
+  def update
+    activity = Activity.find_by(id: params[:id])
+    activity.update_attributes!(activity_params)
+    redirect_to ("/activity/#{activity.id}")
   end
-end
 
 
-redirect_to ("/activity/#{activity.id}")
+  def show
+  @activity = Activity.find_by(id: params[:id])
+  @date_entry = DateEntry.new
+  @comment = Comment.new
+
+  end
 
 
 
-end
+  def voting
 
-private
+    activity = Activity.find_by(id: params[:actid])
+    unchecked = params[:uncheckedboxes]
+    checked = params[:checkedboxes]
 
-def activity_params
-  params.require(:activity).permit(:name, :description, :starts_at, :ends_at, :image, :location)
-end
+
+    # vote is equal to date_entry id (value of checkboxes, stored as array entries in checked/unchecked):
+    checked.each do |vote|
+      if DateVote.exists?(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
+      datevotechecked  = DateVote.find_by(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
+      DateVote.update(datevotechecked.id, check: true)
+      else
+      @datevotechecked = DateVote.new(check: true)
+      @datevotechecked.activity_id = activity.id
+      @datevotechecked.user_id = current_user.id
+      @datevotechecked.date_entry_id = vote
+      @datevotechecked.save
+
+
+
+      end
+    end
+    unchecked.each do |vote|
+      if DateVote.exists?(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
+      datevoteunchecked  = DateVote.find_by(activity_id: activity.id, user_id: current_user.id, date_entry_id: vote)
+      DateVote.update(datevoteunchecked.id,check: false)
+      end
+    end
+
+    activity.date_entries.each do |d|
+    d.votes = d.date_votes.where(check: true).count
+    d.save
+    end
+
+
+    redirect_to ("/activity/#{activity.id}")
+
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:name, :description, :starts_at, :ends_at, :image, :location)
+  end
 
 
 end
--- db/migrate/20160727081845_create_location_entries.rb
+++ db/migrate/20160727081845_create_location_entries.rb
@@ -2,7 +2,7 @@ class CreateLocationEntries < ActiveRecord::Migration[5.0]
   def change
     create_table :location_entries do |t|
       t.string :location
-      t.integer :votes
+      t.integer :votes, :default => 0
       t.references :activity, index: true
 
       t.timestamps

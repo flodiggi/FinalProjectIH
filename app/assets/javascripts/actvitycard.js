17a339f832702b5d4b2eacd5857ed79f1135d76c
Florian Diegruber <florian@diegruber.net>
2016-08-02 23:21:58 +0200

    expanding hover effect activities for mobile clicks

--- app/assets/javascripts/actvitycard.js
+++ app/assets/javascripts/actvitycard.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
         // $(this).find('.event-overview').fadeToggle(200);
     });
 
+    $('.event-container').click(function(){
+          $(this).find('.event-details').fadeToggle(200);
+          // $(this).find('.event-overview').fadeToggle(200);
+      });
+
 
 
 });

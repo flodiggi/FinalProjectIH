6dd6b7af24e6461aad7f2a3d5380f0b94eb58176
Florian Diegruber <florian@diegruber.net>
2016-08-03 17:40:59 +0200

    homepage improved/ design changes and login modal

--- app/assets/javascripts/actvitycard.js
+++ app/assets/javascripts/actvitycard.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-  $('.event-container').hover(function(){
-        $(this).find('.event-details').fadeToggle(200);
-        // $(this).find('.event-overview').fadeToggle(200);
-    });
+  // $('.event-container').hover(function(){
+  //       $(this).find('.event-details').fadeToggle(200);
+  //       // $(this).find('.event-overview').fadeToggle(200);
+  //   });
 
     $('.event-container').click(function(){
           $(this).find('.event-details').fadeToggle(200);
